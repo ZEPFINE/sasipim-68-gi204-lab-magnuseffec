358bbe3a6a2ef920206ff3bcbdf8ed3c1c170041
ZEPFINE <sasipim.saiyot@gmail.com>
2026-03-10 16:48:31 +0700

    Week9-2

--- Assets/Scripts/TorqueRotate.cs
+++ Assets/Scripts/TorqueRotate.cs
@@ -1,16 +1,27 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class TorqueRotate : MonoBehaviour
 {
+    public float TorqueSpeed;
+
+    private Rigidbody rb;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        
+        if (Keyboard.current.bKey.isPressed)
+        {
+            rb.angularVelocity = new Vector3(0, 0, TorqueSpeed);
+        }
+        else
+        {
+            rb.angularVelocity = Vector3.zero;
+        }
     }
 }
